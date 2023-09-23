fb91a3456f369e17357cfbb62cded6de19d5b971
gabriel.baima <gabriel.baima@pdcase.com.br>
2023-09-23 18:11:39 -0300

    -commit inicial

---
+++ src/test/java/com/example/vendas/VendasApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println('ola mundo');
+        assert 
+    }
+
+}
